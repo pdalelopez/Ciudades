eeb3682a97b88867a953933ec063419a7f54d9e6
pdalelopez <pdalelopez@users.noreply.github.com>
2020-03-19 15:33:14 +0100

    Fase 4 ready

--- src/Ciudades.java
+++ src/Ciudades.java
@@ -55,35 +55,59 @@ public class Ciudades {
 		char[] arrayCity6= new char[arrayCities[5].length()];
 		
 		for (int i=0; i<arrayCity1.length;i++) {
-			arrayCity1[i]=arrayCities[0].charAt(i);
-			
-		}
-		for (int i=0; i<arrayCity2.length;i++) {
-			arrayCity2[i]=arrayCities[1].charAt(i);
-			
-		}
-		for (int i=0; i<arrayCity3.length;i++) {
-			arrayCity3[i]=arrayCities[2].charAt(i);
-			
-		}
-		for (int i=0; i<arrayCity4.length;i++) {
-			arrayCity4[i]=arrayCities[3].charAt(i);
-			
-		}
-		for (int i=0; i<arrayCity5.length;i++) {
-			arrayCity5[i]=arrayCities[4].charAt(i);
-			
-		}
-		for (int i=0; i<arrayCity6.length;i++) {
-			arrayCity6[i]=arrayCities[5].charAt(i);
-			
+			arrayCity1[i]=arrayCities[0].charAt(i);	
 		}
 		System.out.println(Arrays.toString(arrayCity1));
+		// invirtiendo el string haciendo uso de la clase stringBuilder
+				String invert1 = new StringBuilder (arrayCities[0]).reverse().toString();
+				System.out.println(invert1);
+		
+		
+		
+		for (int i=0; i<arrayCity2.length;i++) {
+			arrayCity2[i]=arrayCities[1].charAt(i);
+		}
 		System.out.println(Arrays.toString(arrayCity2));
+		// invirtiendo el string haciendo uso de la clase stringBuilder
+				String invert2 = new StringBuilder (arrayCities[1]).reverse().toString();
+				System.out.println(invert2);
+		
+		
+		for (int i=0; i<arrayCity3.length;i++) {
+			arrayCity3[i]=arrayCities[2].charAt(i);
+		}
 		System.out.println(Arrays.toString(arrayCity3));
+		// invirtiendo el string haciendo uso de la clase stringBuilder
+				String invert3 = new StringBuilder (arrayCities[2]).reverse().toString();
+				System.out.println(invert3);
+		
+		
+		for (int i=0; i<arrayCity4.length;i++) {
+			arrayCity4[i]=arrayCities[3].charAt(i);
+		}
 		System.out.println(Arrays.toString(arrayCity4));
+		// invirtiendo el string haciendo uso de la clase stringBuilder
+		String invert4 = new StringBuilder (arrayCities[3]).reverse().toString();
+		System.out.println(invert4);
+		
+		
+		for (int i=0; i<arrayCity5.length;i++) {
+			arrayCity5[i]=arrayCities[4].charAt(i);
+		}
 		System.out.println(Arrays.toString(arrayCity5));
+		// invirtiendo el string haciendo uso de la clase stringBuilder
+		String invert5 = new StringBuilder (arrayCities[4]).reverse().toString();
+		System.out.println(invert5);
+		
+		
+		for (int i=0; i<arrayCity6.length;i++) {
+			arrayCity6[i]=arrayCities[5].charAt(i);
+		}
 		System.out.println(Arrays.toString(arrayCity6));
+		// invirtiendo el string haciendo uso de la clase stringBuilder
+		String invert6 = new StringBuilder (arrayCities[05]).reverse().toString();
+		System.out.println(invert6);
+		
 		
 
 	}
